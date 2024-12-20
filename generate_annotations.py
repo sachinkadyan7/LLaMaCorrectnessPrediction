a979ce667fe223d110f7b19e3c107152a9632e6a
Sachin Kadyan <sachinkadyan7@gmail.com>
2024-12-19 20:17:26 -0500

    New experiment: Classify 4 layers only

--- generate_annotations.py
+++ generate_annotations.py
@@ -12,7 +12,7 @@ for f in root.glob('*/*/attentions/*_attentions.pt'):
     if f.is_file() and f.stat().st_size > 2e6:  # Exclude the empty files
         files_list.append(f.relative_to(root))
         q_id = str(f.stem).split('_')[0]
-        ans_file = next(iter(f.parent.parent.glob("*_answers.json")))
+        ans_file = next(iter(f.parent.parent.glob("*.json")))
         with open(ans_file, "r") as infile:
             ans_json = json.load(infile)
             full_ans = ans_json[q_id]["Full Answer"]
